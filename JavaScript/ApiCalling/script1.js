fd9897a772c9df8bb5aa5f642bb1c14289dbf1e6
prabhatchauhan04 <prabhat14chauhan2004@gmail.com>
2025-08-23 17:52:36 +0530

    Api Calling

---
+++ JavaScript/ApiCalling/script1.js
@@ -0,0 +1,35 @@
+
+let btn = document.querySelector('button');
+
+// Fetch API Example
+let URL = 'https://meowfacts.herokuapp.com/';
+
+function loadData(data) {
+    let p = document.createElement('p');
+    p.innerText = data[0];
+    document.body.appendChild(p);
+}
+
+function getData(URL) {
+    // promise ki form mein kr rhe bcoz har baar website data de hi thodi degi . ho sakta hai down ho ya kuch aur issue ho kabhi .
+    // aur async task hai ye to ye bhi ho sakta hai ki data aane mein time lag jaye to hum promise use krte hai taki jab data aa jaye tab hum uske sath kuch kar ske
+    // fetch ek promise return krta hai
+    fetch(URL)
+        .then((response) => response.json())
+        .then(({data}) => {
+            console.log(data);
+            loadData(data);
+        })
+        .catch(error => {
+            console.error('Error:', error)
+        });
+}
+
+btn.addEventListener('click', function() {
+    getData(URL);
+});
+
+
+
+
+
---
+++ JavaScript/ApiCalling/script2.js
@@ -0,0 +1,27 @@
+
+let btn = document.querySelector('button');
+
+// async await Example
+let URL = 'https://meowfacts.herokuapp.com/';
+
+function loadData(data) {
+    let p = document.createElement('p');
+    p.innerText = data[0];
+    document.body.appendChild(p);
+}
+
+// agar yha error aaya lets say fetch krra but nhi hua toh yha hum uske liye try catch use krte hai
+async function getData(URL) {
+    // ye asynchronous calls hai
+    try{
+    let res = await fetch(URL); // jab tk fetch complete nhi hota tab tk ye ruk jaega bcoz of await 
+    let {data} = await res.json(); // await wali line k neeche code nhi chalega jab tk ye complete nhi hota bcoz of await
+    loadData(data);
+    } catch(error) {
+        console.log('Error aaya hai : ', error);
+    }
+}
+
+btn.addEventListener('click', function() {
+    getData(URL);
+});
